5a35a51c1e9dd5cff60d68aae9abbfe26648907a
Arnav Raviraj <arnavraviraj@gmail.com>
2022-10-12 22:09:28 -0700

    Please enter the commit message for your changes. Lines startint statg with '#' will be ignored, and an empty message aborts the commit

--- Timer.java
+++ Timer.java
@@ -3,6 +3,10 @@ public class Timer {
 	private universityPortal universityPortal;
 
 	public void timerFunc() {
+		
+		universityPortal updatesOnPolicies = universityPortal.getUpdates();
+		
+		updatesOnPolicies.sendNotification();
 
 	}
 
